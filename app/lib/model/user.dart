782da73318b1f413a77a8d0c712a8d21e5a171a3
Duy Duc Nguyen <duyduck2809@gmail.com>
2023-12-02 21:07:10 +0700

    create User model

--- app/lib/model/user.dart
+++ app/lib/model/user.dart
@@ -1,4 +1,33 @@
 class User {
-  String? name;
   String? userName;
+  String? name;
+  String? phoneNumber;
+  String? birthday;
+  int? gender;
+  String? email;
+  String? password;
+  List<String>? address;
+
+  User(
+      {this.userName,
+      this.name,
+      this.phoneNumber,
+      this.birthday,
+      this.gender,
+      this.email,
+      this.password,
+      this.address});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      userName: json['user_name'],
+      name: json['name'],
+      phoneNumber: json['phone_number'],
+      birthday: json['birthDay'],
+      gender: json['gender'],
+      email: json['email'],
+      password: json['password'],
+      address: json['address'],
+    );
+  }
 }
